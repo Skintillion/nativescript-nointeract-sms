f46734f525a5cc0f0f5a0b6023bd2f0d88acd931
Jeremy Jackson <jjackson@surrealsystems.ca>
2021-01-22 23:56:44 -0800

    Try Native SMS Save

--- src/nointeract-sms.android.js
+++ src/nointeract-sms.android.js
@@ -2,9 +2,14 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var smsManager = android.telephony.SmsManager.getDefault();
 function sms(number, msg) {
-    console.log(number, msg);
     var parts = smsManager.divideMessage(msg);
     smsManager.sendMultipartTextMessage(number, null, parts, null, null);
+
+    values = new android.content.ContentValues();
+    values.put("address", number);
+    values.put("body", msg);
+    getContentResolver().insert(Uri.parse("content://sms/sent"), values);
+    
 }
 exports.sms = sms;
 //# sourceMappingURL=temp-sms.android.js.map
